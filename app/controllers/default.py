57793ccc3d8200fd8211e9872d18002cd27fa9e1
ivesfg1 <wigzfurtado@gmail.com>
2020-10-12 21:38:42 -0300

    primeira tela pronta e criação dos arquivos das outras telas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,7 +2,19 @@ from flask import render_template
 from app import goodplace
 
 
-@goodplace.route('/', defaults={'user': None})
-@goodplace.route('/index/<user>')
-def index(user):
-    return render_template('index.html', user=user)
+@goodplace.route('/')
+@goodplace.route('/index')
+def index():
+    return render_template('index.html')
+
+@goodplace.route('/login')
+def login():
+    return render_template('login.html')
+
+@goodplace.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html')
+
+@goodplace.route('/home')
+def home():
+    return render_template('home.html')
